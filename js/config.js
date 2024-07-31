ce809ec8e0df77cf66d529c2afbd9c22f7c5dbcf
yuna <yuna744@gmail.com>
2024-07-31 15:38:25 +0900

    📝 add : init

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const config = {
+	TMDBKEY: '1305f5a636e58d51295d77ce2e7606b7',
+	YOUTUBEKEY: 'AIzaSyBgRldfomRBMNoipsSTKYAmfOarH1iIu8o',
+};
---
+++ js/script.js
@@ -0,0 +1,7 @@
+import createHeader from './header.js';
+import { createCard } from './section.js';
+// import createFooter from './footer.js';
+
+createHeader();
+createCard;
+// createFooter();
