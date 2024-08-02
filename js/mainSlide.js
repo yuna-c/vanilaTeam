7221db90b05c9d6ce1fa6841eec95f85f5d61e79
limgi <dntksql1010@naver.com>
2024-08-03 00:41:01 +0900

    🩹 fix 메인 슬라이드 js, css 변수, css 수정

--- js/mainSlide.js
+++ js/mainSlide.js
@@ -1,57 +1,56 @@
 import { getPopularData} from './fetchData.js';
 
 export const creatSlideBox = async() => {
+    const data = await getPopularData(1);
+
     const app = document.getElementById('app');
-    const banner = document.createElement('section');
+    const contentBox = document.createElement('section');
     const text = document.createElement('p');
     const slideWrap = document.createElement('div');
     const slideBox = document.createElement('div');
     const slideList = document.createElement('ul');
-    
-    text.innerHTML = `영화 리뷰 페이지<br>리뷰, 줄거리, 평점까지 한번에 다<br>여기서 확인하세요`
-    
-    const data = await getPopularData(1);
-    const slideData = data.results.slice(1, 10);
-    
-    console.log(slideData[0]);
-    console.log('mainbanner');
-    
-    banner.classList = 'banner';
+    const slideItem = creatSlideItem(data);
+
+    contentBox.classList = 'content';
     slideWrap.classList = 'slide_wrap';
     slideBox.classList = 'slide_box';
     slideList.classList = 'slide_list';
-    
-    app.appendChild(banner).appendChild(text);
-    banner.appendChild(slideWrap).appendChild(slideBox).appendChild(slideList);
-    slideList.innerHTML += creatSlide(data);
+    text.classList = 'txt';
 
-    const slideItem = creatSlide(data);
+    text.innerHTML = `영화 리뷰 페이지<br>리뷰, 줄거리, 평점까지 한번에 다<br>여기서 확인하세요`
+
+    console.log('mainbanner');
+    console.log(slideItem);
     
-    const slider = slideWrap.querySelectorAll(".slide_item");
-    slide(slider)
+    app.appendChild(contentBox).appendChild(text);
+    contentBox.appendChild(slideWrap).appendChild(slideBox).appendChild(slideList);
+    slideList.innerHTML += creatSlideItem(data);
+    const arrSlideList = slideWrap.querySelectorAll(".slide_item");
+    
+    sliding(arrSlideList);
 }
 
-const creatSlide = (data) => {
+const creatSlideItem = (data) => {
     const dataResult = data.results.slice(0,10);
     let slidItem = dataResult.map((item)=>{
         return `
         <li class="slide_item">
-            <img src="https://image.tmdb.org/t/p/w500${item.backdrop_path}" alt="${item.title}">
-        <li>
+            <img src="https://image.tmdb.org/t/p/w1280${item.backdrop_path}" alt="${item.title}">
+        </li>
         `
-    });
+    }).join('');
     return slidItem;
 }
 
-const slide = (a) =>{
+const sliding = (arrSlideList) =>{
     let currentIndex = 0;               // 현재 보이는 이미지
-    let sliderCount = a.length;    // 이미지 갯수
+    let sliderCount = arrSlideList.length;    // 이미지 갯수
     let sliderInterval = 3000;          // 이미지 변경 간격 시간
-    
+
     setInterval(() => {
         let nextIndex = (currentIndex + 1) % sliderCount;
-        a[currentIndex].style.opacity = "0";
-        a[nextIndex].style.opacity = "1";
+        arrSlideList[currentIndex].style.opacity = "0";
+        arrSlideList[nextIndex].style.opacity = "1";
         currentIndex = nextIndex;
     }, sliderInterval);
 }
